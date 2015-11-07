b8c0e23c92b845be0ead8f0f865f1be8aeb78352
Jeff Dyer <jeffdyer@acm.org>
2015-11-07 14:15:01 -0800

    Fix build

--- Makefile
+++ Makefile
@@ -1,14 +1,19 @@
 SRC = $(wildcard src/*.js)
 LIB = $(SRC:src/%.js=lib/%.js)
+PUB = $(LIB:lib/%.js=pub/%.js)
 
-default: lib run
+default: lib pub run
 
 lib: $(LIB)
+pub: $(PUB)
 lib/%.js: src/%.js
 	mkdir -p $(@D)
 	babel --modules common $< -o $@
 
+pub/%.js: lib/%.js
+	mkdir -p $(@D)
+	browserify lib/assert.js lib/viewer.js > pub/viewer.js
+
 run:
-	browserify src/viewer/viewer.js -t babelify --outfile pub/viewer.js
 	npm start
 
--- pub/viewer.js
+++ pub/viewer.js
@@ -1,4 +1,211 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/* -*- Mode: js; js-indent-level: 2; indent-tabs-mode: nil; tab-width: 2 -*- */
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+/*
+ * Copyright 2013 Art Compiler LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+"use strict"
+/*
+  ASSERTS AND MESSAGES
+
+  We use the 'assert()' function to trap invalid states of all kinds. External
+  messages are distinguished from internal messages by a numeric prefix that
+  indicates the error code associated with the message. For example, the
+  following two asserts implement an internal and external assert, respectively.
+
+     assert(false, "This code is broken.");
+     assert(false, "1001: Invalid user input.");
+
+  To aid in the writing of external messages, we keep them in a single global
+  table named 'messages'. Each module adds to this table its own messages
+  with an expression such as
+
+     messages[1001] = "Invalid user input.";
+
+  These messages are accessed with the 'message' function as such
+
+     message(1001);
+
+  Calling 'assert' with 'message' looks like
+
+     assert(x != y, message(1001));
+
+  ALLOCATING ERROR CODES
+
+  In order to avoid error code conflicts, each module claims a range of values
+  that is not already taken by the modules in the same system. A module claims
+  a range of codes by calling the function reserveCodeRange() like this:
+
+     reserveCodeRange(1000, 1999, "mymodule");
+
+  If the requested code range has any values that are already reserved, then
+  an assertion is raised.
+
+  USAGE
+
+  In general, only allocate message codes for external asserts. For internal
+  asserts, it is sufficient to simply inline the message text in the assert
+  expression.
+
+  It is good to write an assert for every undefined state, regardless of whether
+  it is the result of external input or not. Asserts can then be externalized if
+  and when they it is clear that they are the result of external input.
+
+  A client module can override the messages provided by the libraries it uses by
+  simply redefining those messages after the defining library is loaded. That is,
+  the client can copy and past the statements of the form
+
+     messages[1001] = "Invalid user input.";
+
+  and provide new text for the message.
+
+     messages[1001] = "Syntax error.";
+
+  In the same way different sets of messages can be overridden for the purpose
+  of localization.
+
+*/
+
+;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var location = "";
+var messages = {};
+var reservedCodes = [];
+var ASSERT = true;
+var assert = (function () {
+  return !ASSERT ? function () {} : function (val, str) {
+    if (str === void 0) {
+      str = "failed!";
+    }
+    if (!val) {
+      var err = new Error(str);
+      err.location = location;
+      throw err;
+    }
+  };
+})();
+
+var message = function message(errorCode, args) {
+  var str = messages[errorCode];
+  if (args) {
+    args.forEach(function (arg, i) {
+      str = str.replace("%" + (i + 1), arg);
+    });
+  }
+  return errorCode + ": " + str;
+};
+
+var reserveCodeRange = function reserveCodeRange(first, last, moduleName) {
+  assert(first <= last, "Invalid code range");
+  var noConflict = reservedCodes.every(function (range) {
+    return last < range.first || first > range.last;
+  });
+  assert(noConflict, "Conflicting request for error code range");
+  reservedCodes.push({ first: first, last: last, name: moduleName });
+};
+
+var setLocation = function setLocation(location) {
+  //assert(location, "Empty location");
+  location = loc;
+};
+
+var clearLocation = function clearLocation() {
+  location = null;
+};
+
+var setCounter = function setCounter(n, message) {
+  count = n;
+  countMessage = message ? message : "ERROR count exceeded";
+};
+
+var checkCounter = function checkCounter() {
+  if (typeof count !== "number" || isNaN(count)) {
+    assert(false, "ERROR counter not set");
+    return;
+  }
+  assert(count--, countMessage);
+};
+
+exports.assert = assert;
+exports.message = message;
+exports.messages = messages;
+exports.reserveCodeRange = reserveCodeRange;
+
+},{}],2:[function(require,module,exports){
+"use strict";
+
+var _assert = require("./assert");
+
+var _topojson = require("topojson");
+
+var topojson = _interopRequireWildcard(_topojson);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+/* -*- Mode: js; js-indent-level: 2; indent-tabs-mode: nil; tab-width: 2 -*- */
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+/* Copyright (c) 2015, Jeff Dyer, Art Compiler LLC */
+//import {d3} from "./d3.v3.js";
+
+window.exports.viewer = (function () {
+  function update(el, obj, src, pool) {
+    var data = undefined;
+    (0, _assert.assert)(obj, "No data found!");
+    obj = JSON.parse(obj);
+    var str = undefined;
+    if (obj.error && obj.error.length > 0) {
+      str = "ERROR";
+      data = [];
+    } else {
+      data = obj.data;
+    }
+    var textSpan = "";
+    var style = "";
+    var height = 24;
+    data.forEach(function (data) {
+      style = "";
+      str = data.value;
+      if (data.style) {
+        data.style.forEach(function (p) {
+          style += p.key[0] + ":" + p.val.value + ";";
+          if (p.key === "font-size") {
+            height = p.val;
+          }
+        });
+      }
+      textSpan += "<tspan style='" + style + "'>" + str + "</tspan> ";
+    });
+    var text = "<text x='4' y='" + height + "'>" + textSpan + "</text>";
+    $(el).html('<g>' + text + '</g>');
+    var bbox = $("#graff-view svg g")[0].getBBox();
+    $(el).attr("height", bbox.height + 12 + "px");
+    $(el).attr("width", bbox.width + 10 + "px");
+  }
+  function capture(el) {
+    var mySVG = $(el).html();
+    return mySVG;
+  }
+  return {
+    update: update,
+    capture: capture
+  };
+})();
+
+},{"./assert":1,"topojson":3}],3:[function(require,module,exports){
 !function() {
   var topojson = {
     version: "1.6.19",
@@ -534,209 +741,4 @@
   else this.topojson = topojson;
 }();
 
-},{}],2:[function(require,module,exports){
-/* -*- Mode: js; js-indent-level: 2; indent-tabs-mode: nil; tab-width: 2 -*- */
-/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
-/*
- * Copyright 2013 Art Compiler LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-"use strict";
-/*
-  ASSERTS AND MESSAGES
-
-  We use the 'assert()' function to trap invalid states of all kinds. External
-  messages are distinguished from internal messages by a numeric prefix that
-  indicates the error code associated with the message. For example, the
-  following two asserts implement an internal and external assert, respectively.
-
-     assert(false, "This code is broken.");
-     assert(false, "1001: Invalid user input.");
-
-  To aid in the writing of external messages, we keep them in a single global
-  table named 'messages'. Each module adds to this table its own messages
-  with an expression such as
-
-     messages[1001] = "Invalid user input.";
-
-  These messages are accessed with the 'message' function as such
-
-     message(1001);
-
-  Calling 'assert' with 'message' looks like
-
-     assert(x != y, message(1001));
-
-  ALLOCATING ERROR CODES
-
-  In order to avoid error code conflicts, each module claims a range of values
-  that is not already taken by the modules in the same system. A module claims
-  a range of codes by calling the function reserveCodeRange() like this:
-
-     reserveCodeRange(1000, 1999, "mymodule");
-
-  If the requested code range has any values that are already reserved, then
-  an assertion is raised.
-
-  USAGE
-
-  In general, only allocate message codes for external asserts. For internal
-  asserts, it is sufficient to simply inline the message text in the assert
-  expression.
-
-  It is good to write an assert for every undefined state, regardless of whether
-  it is the result of external input or not. Asserts can then be externalized if
-  and when they it is clear that they are the result of external input.
-
-  A client module can override the messages provided by the libraries it uses by
-  simply redefining those messages after the defining library is loaded. That is,
-  the client can copy and past the statements of the form
-
-     messages[1001] = "Invalid user input.";
-
-  and provide new text for the message.
-
-     messages[1001] = "Syntax error.";
-
-  In the same way different sets of messages can be overridden for the purpose
-  of localization.
-
-*/
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var location = "";
-var messages = {};
-var reservedCodes = [];
-var ASSERT = true;
-var assert = (function () {
-  return !ASSERT ? function () {} : function (val, str) {
-    if (str === void 0) {
-      str = "failed!";
-    }
-    if (!val) {
-      var err = new Error(str);
-      err.location = location;
-      throw err;
-    }
-  };
-})();
-
-var message = function message(errorCode, args) {
-  var str = messages[errorCode];
-  if (args) {
-    args.forEach(function (arg, i) {
-      str = str.replace("%" + (i + 1), arg);
-    });
-  }
-  return errorCode + ": " + str;
-};
-
-var reserveCodeRange = function reserveCodeRange(first, last, moduleName) {
-  assert(first <= last, "Invalid code range");
-  var noConflict = reservedCodes.every(function (range) {
-    return last < range.first || first > range.last;
-  });
-  assert(noConflict, "Conflicting request for error code range");
-  reservedCodes.push({ first: first, last: last, name: moduleName });
-};
-
-var setLocation = function setLocation(location) {
-  //assert(location, "Empty location");
-  location = loc;
-};
-
-var clearLocation = function clearLocation() {
-  location = null;
-};
-
-var setCounter = function setCounter(n, message) {
-  count = n;
-  countMessage = message ? message : "ERROR count exceeded";
-};
-
-var checkCounter = function checkCounter() {
-  if (typeof count !== "number" || isNaN(count)) {
-    assert(false, "ERROR counter not set");
-    return;
-  }
-  assert(count--, countMessage);
-};
-
-exports.assert = assert;
-exports.message = message;
-exports.messages = messages;
-exports.reserveCodeRange = reserveCodeRange;
-
-},{}],3:[function(require,module,exports){
-/* -*- Mode: js; js-indent-level: 2; indent-tabs-mode: nil; tab-width: 2 -*- */
-/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
-/* Copyright (c) 2015, Jeff Dyer, Art Compiler LLC */
-//import {d3} from "./d3.v3.js";
-"use strict";
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj["default"] = obj; return newObj; } }
-
-var _assert = require("./assert");
-
-var _topojson = require("topojson");
-
-var topojson = _interopRequireWildcard(_topojson);
-
-window.exports.viewer = (function () {
-  function update(el, obj, src, pool) {
-    var data = undefined;
-    (0, _assert.assert)(obj, "No data found!");
-    obj = JSON.parse(obj);
-    var str = undefined;
-    if (obj.error && obj.error.length > 0) {
-      str = "ERROR";
-      data = [];
-    } else {
-      data = obj.data;
-    }
-    var textSpan = "";
-    var style = "";
-    var height = 24;
-    data.forEach(function (data) {
-      style = "";
-      str = data.value;
-      if (data.style) {
-        data.style.forEach(function (p) {
-          style += p.key[0] + ":" + p.val.value + ";";
-          if (p.key === "font-size") {
-            height = p.val;
-          }
-        });
-      }
-      textSpan += "<tspan style='" + style + "'>" + str + "</tspan> ";
-    });
-    var text = "<text x='4' y='" + height + "'>" + textSpan + "</text>";
-    $(el).html('<g>' + text + '</g>');
-    var bbox = $("#graff-view svg g")[0].getBBox();
-    $(el).attr("height", bbox.height + 12 + "px");
-    $(el).attr("width", bbox.width + 10 + "px");
-  }
-  function capture(el) {
-    var mySVG = $(el).html();
-    return mySVG;
-  }
-  return {
-    update: update,
-    capture: capture
-  };
-})();
-
-},{"./assert":2,"topojson":1}]},{},[3]);
+},{}]},{},[1,2]);
