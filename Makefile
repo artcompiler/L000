5dec8750886568aacf3a147627b7369651674c28
Jeff Dyer <jeffdyer@acm.org>
2016-01-02 11:34:51 -0700

    Render viewer as React component

--- Makefile
+++ Makefile
@@ -2,13 +2,15 @@ SRC = $(wildcard src/*.js)
 LIB = $(SRC:src/%.js=lib/%.js)
 PUB = $(LIB:lib/%.js=pub/%.js)
 
-default: lib pub run
+#default: lib pub run
+default: lib run
 
 lib: $(LIB)
 pub: $(PUB)
 lib/%.js: src/%.js
 	mkdir -p $(@D)
-	babel --modules common $< -o $@
+#	babel --modules common $< -o $@
+	browserify -t babelify src/viewer.js > pub/viewer.js
 
 pub/%.js: lib/%.js
 	mkdir -p $(@D)
--- src/viewer.js
+++ src/viewer.js
@@ -3,49 +3,33 @@
 /* Copyright (c) 2015, Jeff Dyer, Art Compiler LLC */
 //import {d3} from "./d3.v3.js";
 import {assert, message, messages, reserveCodeRange} from "./assert";
-import * as topojson from "topojson";
-
+import * as React from "./react";
 window.exports.viewer = (function () {
-  function update(el, obj, src, pool) {
-    let data;
-    assert(obj, "No data found!");
-    obj = JSON.parse(obj);
-    let str;
-    if (obj.error && obj.error.length > 0) {
-      str = "ERROR";
-      data = [];
-    } else {
-      data = obj.data;
-    }
-    var textSpan = "";
-    var style = ""
-    var height = 24;
-    data.forEach(function (data) {
-      style = "";
-      str = data.value;
-      if (data.style) {
-        data.style.forEach(function (p) {
-          style += p.key[0] + ":" + p.val.value + ";";
-          if (p.key === "font-size") {
-            height = p.val;
-          }
-        });
-      }
-      textSpan += "<tspan style='" + style + "'>" + str + "</tspan> ";
-    });
-    var text = "<text x='4' y='" + height + "'>" + textSpan + "</text>";
-    $(el).html('<g>' + text + '</g>');
-    var bbox = $("#graff-view svg g")[0].getBBox();
-    $(el).attr("height", (bbox.height + 12) + "px");
-    $(el).attr("width", (bbox.width + 10) + "px");
-  }
   function capture(el) {
     var mySVG = $(el).html();
     return mySVG;
   }
+  var Viewer = React.createClass({
+    render: function () {
+      var data = this.props.data ? this.props.data : [];
+      var elts = [];
+      data.forEach(function (d, i) {
+        var style = {};
+        if (d.style) {
+          d.style.forEach(function (p) {
+            style[p.key[0]] = p.val.value;
+          });
+        }
+        elts.push(<span key={i} style={style}>{d.value}</span>);
+      });
+      return (
+        <div>{elts}</div>
+      );
+    },
+  });
   return {
-    update: update,
     capture: capture,
+    Viewer: Viewer
   };
 })();
 
