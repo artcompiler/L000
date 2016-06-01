2dcef839c8beee6a58911c310767bc7928c48c76
Jeff Dyer <jeffdyer@acm.org>
2016-06-01 11:04:50 -0600

    More cleanup

--- pub/lexicon.js
+++ pub/lexicon.js
@@ -3,6 +3,5 @@ exports.globalLexicon = {
     "add" : { "tk": 1, "name": "ADD", "cls": "function", "length": 2 , "arity": 2 },
     "style" : { "tk": 1, "name": "STYLE", "cls": "function", "length": 2, "arity": 2 },
     // values
-    "timer" : { "tk": 1, "cls": "string", "val": "$$timer$$", "length": 0 },
-    "prose" : { "tk": 1, "cls": "string", "val": "$$prose$$", "length": 0 }
+    "timer" : { "tk": 1, "cls": "string", "val": "$$timer$$", "length": 0 }
 }
