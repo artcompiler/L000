ee227a56409392e18522c26ea8252a2fe5498a5a
Jeff Dyer <jeffdyer@acm.org>
2016-07-26 11:57:08 -0600

    Fix lexicon

--- pub/lexicon.js
+++ pub/lexicon.js
@@ -1,9 +1,7 @@
-"use strict";
-
 exports.globalLexicon = {
     // functions
-    "add": { "tk": 1, "name": "ADD", "cls": "function", "length": 2, "arity": 2 },
-    "style": { "tk": 1, "name": "STYLE", "cls": "function", "length": 2, "arity": 2 },
+    "add" : { "tk": 1, "name": "ADD", "cls": "function", "length": 2 , "arity": 2 },
+    "style" : { "tk": 1, "name": "STYLE", "cls": "function", "length": 2, "arity": 2 },
     // values
-    "timer": { "tk": 1, "cls": "string", "val": "$$timer$$", "length": 0 }
-};
+    "timer" : { "tk": 1, "cls": "string", "val": "$$timer$$", "length": 0 }
+}
