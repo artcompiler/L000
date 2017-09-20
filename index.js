191a55d8ffba3c786b22a0d0a034d04bcdc00c6d
Jeff Dyer <jeffdyer@acm.org>
2017-09-19 17:08:49 -0700

    Init support for authorization

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
    L000 compiler service.
    @flow weak
 */
-var express = require('express')
-var compiler = require("./lib/compile.js");
-var app = express();
+const https = require("https");
+const express = require('express')
+const compiler = require("./lib/compile.js");
+const app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/pub'));
 app.get("/.well-known/acme-challenge/S4EDKpiNxlbJT1sGyWksMQ28FXJdXcn553EhoINNW-U", function(req, res) {
@@ -16,6 +17,61 @@ app.get('/', function(req, res) {
 app.get("/version", function(req, res) {
   res.send(compiler.version ? compiler.version : "v0.0.0");
 });
+
+function postAuth(path, data, resume) {
+  let encodedData = JSON.stringify(data);
+  var options = {
+    host: "auth.artcompiler.com",
+    port: "443",
+    path: path,
+    method: "POST",
+    headers: {
+      'Content-Type': 'text/plain',
+      'Content-Length': Buffer.byteLength(encodedData),
+    },
+  };
+  var req = https.request(options);
+  req.on("response", (res) => {
+    var data = "";
+    res.on('data', function (chunk) {
+      data += chunk;
+    }).on('end', function () {
+      try {
+        resume(null, JSON.parse(data));
+      } catch (e) {
+        console.log("ERROR " + data);
+        console.log(e.stack);
+      }
+    }).on("error", function () {
+      console.log("error() status=" + res.statusCode + " data=" + data);
+    });
+  });
+  req.end(encodedData);
+  req.on('error', function(err) {
+    console.log("ERROR " + err);
+    resume(err);
+  });
+}
+
+const validated = {};
+function validate(token, resume) {
+  if (token === undefined) {
+    resume(null, {
+      address: "guest",
+      access: "compile",
+    });
+  } else if (validated[token]) {
+    resume(null, validated[token]);
+  } else {
+    postAuth("/validate", {
+      jwt: token
+    }, (err, data) => {
+      validated[token] = data;
+      resume(err, data);
+    });
+  }
+}
+
 app.get("/compile", function(req, res) {
   let body = "";
   req.on("data", function (chunk) {
@@ -23,17 +79,24 @@ app.get("/compile", function(req, res) {
   });
   req.on('end', function () {
     body = JSON.parse(body);
-    let code = body.src;
-    let data = body.data;
-    let t0 = new Date;
-    let obj = compiler.compile(code, data, function (err, val) {
-      if (err.length) {
-        res.send({
-          error: err,
-        });
+    let auth = body.auth;
+    validate(auth, (err, data) => {
+      if (err) {
+        res.send(err);
       } else {
-        console.log("GET /compile " + (new Date - t0) + "ms");
-        res.json(val);
+        let code = body.src;
+        let data = body.data;
+        let t0 = new Date;
+        let obj = compiler.compile(code, data, function (err, val) {
+          if (err.length) {
+            res.send({
+              error: err,
+            });
+          } else {
+            console.log("GET /compile " + (new Date - t0) + "ms");
+            res.json(val);
+          }
+        });
       }
     });
   });
