ccb5353a88b1d71996ed4617ff5d8b767136a7cb
Jeff Dyer <jeffdyer@acm.org>
2017-09-21 12:47:06 -0700

    Share code

--- index.js
+++ index.js
@@ -6,79 +6,22 @@ const https = require("https");
 const express = require('express')
 const compiler = require("./lib/compile.js");
 const app = express();
-app.set('port', (process.env.PORT || 5000));
+const langID = "000";
+// SHARED START
+app.set('port', (process.env.PORT || "5" + langID));
 app.use(express.static(__dirname + '/pub'));
-app.get("/.well-known/acme-challenge/S4EDKpiNxlbJT1sGyWksMQ28FXJdXcn553EhoINNW-U", function(req, res) {
-  res.send("S4EDKpiNxlbJT1sGyWksMQ28FXJdXcn553EhoINNW-U.Fzpon67yOJjoArf9Yosy2tR5vF2zLd5fJ3tSglCuLoI");
-});
 app.get('/', function(req, res) {
-  res.send("Hello, L000!");
+  res.send("Hello, L" + langID + "!");
+});
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
 });
 app.get("/version", function(req, res) {
-  res.send(compiler.version ? compiler.version : "v0.0.0");
+  res.send(compiler.version || "v0.0.0");
 });
-function postAuth(path, data, resume) {
-  let encodedData = JSON.stringify(data);
-  var options = {
-    host: "auth.artcompiler.com",
-    port: "443",
-    path: path,
-    method: "POST",
-    headers: {
-      'Content-Type': 'text/plain',
-      'Content-Length': Buffer.byteLength(encodedData),
-    },
-  };
-  var req = https.request(options);
-  req.on("response", (res) => {
-    var data = "";
-    res.on('data', function (chunk) {
-      data += chunk;
-    }).on('end', function () {
-      try {
-        resume(null, JSON.parse(data));
-      } catch (e) {
-        console.log("ERROR " + data);
-        console.log(e.stack);
-      }
-    }).on("error", function () {
-      console.log("error() status=" + res.statusCode + " data=" + data);
-    });
-  });
-  req.end(encodedData);
-  req.on('error', function(err) {
-    console.log("ERROR " + err);
-    resume(err);
-  });
-}
-function count(token, count) {
-  postAuth("/count", {
-    jwt: token,
-    lang: "L000",
-    count: count,
-  }, () => {});
-}
-const validated = {};
-function validate(token, resume) {
-  if (token === undefined) {
-    resume(null, {
-      address: "guest",
-      access: "compile",
-    });
-  } else if (validated[token]) {
-    resume(null, validated[token]);
-    count(token, 1);
-  } else {
-    postAuth("/validate", {
-      jwt: token,
-      lang: "L000",
-    }, (err, data) => {
-      validated[token] = data;
-      resume(err, data);
-      count(token, 1);
-    });
-  }
-}
 app.get("/compile", function(req, res) {
   let body = "";
   req.on("data", function (chunk) {
@@ -117,9 +60,66 @@ app.get("/compile", function(req, res) {
     res.send(e);
   });
 });
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-});
-process.on('uncaughtException', function(err) {
-  console.log('Caught exception: ' + err);
-});
+function postAuth(path, data, resume) {
+  let encodedData = JSON.stringify(data);
+  var options = {
+    host: "auth.artcompiler.com",
+    port: "443",
+    path: path,
+    method: "POST",
+    headers: {
+      'Content-Type': 'text/plain',
+      'Content-Length': Buffer.byteLength(encodedData),
+    },
+  };
+  var req = https.request(options);
+  req.on("response", (res) => {
+    var data = "";
+    res.on('data', function (chunk) {
+      data += chunk;
+    }).on('end', function () {
+      try {
+        resume(null, JSON.parse(data));
+      } catch (e) {
+        console.log("ERROR " + data);
+        console.log(e.stack);
+      }
+    }).on("error", function () {
+      console.log("error() status=" + res.statusCode + " data=" + data);
+    });
+  });
+  req.end(encodedData);
+  req.on('error', function(err) {
+    console.log("ERROR " + err);
+    resume(err);
+  });
+}
+function count(token, count) {
+  postAuth("/count", {
+    jwt: token,
+    lang: "L" + langID,
+    count: count,
+  }, () => {});
+}
+const validated = {};
+function validate(token, resume) {
+  if (token === undefined) {
+    resume(null, {
+      address: "guest",
+      access: "compile",
+    });
+  } else if (validated[token]) {
+    resume(null, validated[token]);
+    count(token, 1);
+  } else {
+    postAuth("/validate", {
+      jwt: token,
+      lang: "L" + langID,
+    }, (err, data) => {
+      validated[token] = data;
+      resume(err, data);
+      count(token, 1);
+    });
+  }
+}
+// SHARED STOP
