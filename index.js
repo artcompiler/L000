7d4844b2b9f40a61116b6865faeebec45e42b6af
Jeff Dyer <jeffdyer@acm.org>
2017-09-21 14:07:33 -0700

    Share code

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.get("/compile", function(req, res) {
         } else {
           let code = body.src;
           let data = body.data;
+          data.REFRESH = body.refresh; // Stowaway flag.
           let t0 = new Date;
           let obj = compiler.compile(code, data, function (err, val) {
             if (err.length) {
